2424e134929526640c8830f0c570176b49dc1651
rtuttle93 <82906399+rtuttle93@users.noreply.github.com>
2021-09-21 09:48:30 -0400

    Update engineerCardTemplate.js

--- dist/engineerCardTemplate.js
+++ dist/engineerCardTemplate.js
@@ -1,1 +1,14 @@
 
+function engineerCard(data) {
+    return `<div class="card text-white bg-success text-center" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${data.name}</h5>
+      <h6 class="card-subtitle mb-2 text-white">Engineer</h6>
+      <p class="card-text">ID:${data.id}</p>
+      <p class="card-text">E-mail:<a href="mailto:${data.email}"> ${data.email}</a></p>
+      <p class="card-text">GitHub:<a href="https://github.com/${data.github}"> ${data.github}</a></p>
+    </div>
+  </div>`;
+  }
+  
+  module.exports = engineerCard;
