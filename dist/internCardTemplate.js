5863559eb0de91508960094f917eb3dcd8066fbd
rtuttle93 <82906399+rtuttle93@users.noreply.github.com>
2021-09-21 09:47:53 -0400

    Update internCardTemplate.js

--- dist/internCardTemplate.js
+++ dist/internCardTemplate.js
@@ -1,1 +1,14 @@
 
+function internCard(data) {
+  return `<div class="card text-white bg-danger text-center" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${data.name}</h5>
+    <h6 class="card-subtitle mb-2 text-white">Intern</h6>
+    <p class="card-text">ID:${data.id}</p>
+    <p class="card-text">E-mail:<a href="mailto:${data.email}"> ${data.email}</a></p>
+    <p class="card-text">School: ${data.school}</p>
+  </div>
+</div>`;
+}
+
+module.exports = internCard;
